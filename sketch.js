e897d93031d7216a3f3b0cc2f98c995a5e460a94
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-09-08 13:33:17 -0400

    second change

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // put setup code here
+  createCanvas(500,500);
+  background(300,0,0);
+}
+
+function draw() {
+  // put drawing code here
+  fill(0,0,300);
+  rect(20,20,460,460);
+  fill(200,200,0);
+  circle(250,250,450);
+}
