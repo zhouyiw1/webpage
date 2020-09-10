957acbca1b77fd609b0387e172f353a5caa60099
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-09-10 13:25:39 -0400

    1

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ function setup() {
   // put setup code here
   createCanvas(500,500);
   background(300,0,0);
-  angleMode(DEGREES);
 }
 
 function draw() {
@@ -11,5 +10,4 @@ function draw() {
   rect(20,20,460,460);
   fill(200,200,0);
   circle(250,250,450);
-  fill(0);  
 }
