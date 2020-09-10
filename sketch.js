ecdf0a46d355f497da49e5639e35be486c988594
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-09-10 13:13:06 -0400

    ROTATE TEST

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ function setup() {
   // put setup code here
   createCanvas(500,500);
   background(300,0,0);
+  angleMode(DEGREES);
 }
 
 function draw() {
@@ -10,4 +11,6 @@ function draw() {
   rect(20,20,460,460);
   fill(200,200,0);
   circle(250,250,450);
+  rotate(45);
+  rect(40,40,325,325);
 }
