f881462e886b258743ce7f2c9ec0e6896f652f94
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-09-15 14:35:57 -0400

    mouseXY

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
-  background(255,128,191);
   angleMode(DEGREES);
 }
 
 function draw() {
   // put drawing code here
+  background(255,128,191);
   push();
   rotate(45);
   fill(190,231,233);
+  noStroke();
   rect(0,0,250,250);
   pop();
-  fill(200,200,0);
-  circle(249,205,173);
+  fill(249,205,173);
+  noStroke();
+  circle(700,700,900);
+  circle(mouseX,mouseY,30);
+
 }
