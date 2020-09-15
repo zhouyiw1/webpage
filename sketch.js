a030d047bcd2714b567e671391f4d3a8c9f199f7
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-09-15 14:59:56 -0400

    changing color

--- sketch.js
+++ sketch.js
@@ -11,12 +11,12 @@ function draw() {
   rotate(45);
   fill(190,231,233);
   noStroke();
-  rect(350,350,250,250);
+  rect(500,100,250,250);
   pop();
   fill(249,205,173);
   noStroke();
   circle(700,700,900);
-  fill(255,76,108);
+  fill(mouseX,76,108);
   circle(mouseX,mouseY,30);
 
 }
