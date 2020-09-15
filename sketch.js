c73b33110076a7b38e5b8a90efb0a8a7d8de847c
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-09-15 14:38:42 -0400

    mouse change

--- sketch.js
+++ sketch.js
@@ -6,16 +6,17 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(255,128,191);
+  background(255 179 168);
   push();
   rotate(45);
   fill(190,231,233);
   noStroke();
-  rect(0,0,250,250);
+  rect(350,350,250,250);
   pop();
   fill(249,205,173);
   noStroke();
   circle(700,700,900);
+  fill(255,76,108);
   circle(mouseX,mouseY,30);
 
 }
