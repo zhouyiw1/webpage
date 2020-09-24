093872b6167322cc612a3e81a29ffd31e43d7150
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-09-24 12:27:15 -0400

    inclass3

---
+++ InClassAssignment3/sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  // put setup code here
+createCanvas(200,200);
+background(200,10,120);
+
+}
+
+function draw() {
+  //Put draw code here
+  fill(30,40,200);
+  circle(100,100,70);
+
+
+}
