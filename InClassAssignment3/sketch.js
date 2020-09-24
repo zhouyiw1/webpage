6a9aff305a1f5c9f1e3f0b74b2fba32967b363b5
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-09-24 14:22:52 -0400

    test2

--- InClassAssignment3/sketch.js
+++ InClassAssignment3/sketch.js
@@ -7,7 +7,9 @@ function setup() {
 function draw() {
   // put drawing code here
   for(let i=0;i<width;i=i+50){
-    for(let j=0;j=height;j=j+50){
+    for(let j=0;j<height;j=j+50){
+      for(let k=0;k<width;k=k+50){
+        for(let l=25;l<height;l=l+50){
       let rcol=random(150,255);
       let gcol=random(0,255);
       let bcol=random(100,233);
@@ -16,7 +18,9 @@ function draw() {
       rect(i,j,20);
       fill(rcol,gcol,bcol);
       noStroke();
-      circle(i+25,j+25,20);
+      circle(k,l,20);
+       }
+      }
     }
   }
 
