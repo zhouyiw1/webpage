22c1fd872dfe209ce720776a14d8fe702d21fef3
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-09-24 14:00:52 -0400

    test1

--- InClassAssignment3/sketch.js
+++ InClassAssignment3/sketch.js
@@ -1,15 +1,23 @@
-
 function setup() {
   // put setup code here
-createCanvas(200,200);
-background(200,10,120);
-
+  createCanvas(windowWidth,windowHeight);
+  background(255,179,168);
 }
 
 function draw() {
-  //Put draw code here
-  fill(30,40,200);
-  circle(100,100,70);
-
+  // put drawing code here
+  for(let i=0;i<width;i=i+50){
+    for(let j=0;j=height;j=j+50){
+      let rcol=random(150,255);
+      let gcol=random(0,255);
+      let bcol=random(100,233);
+      fill(rcol,gcol,bcol);
+      noStroke();
+      rect(i,j,20);
+      fill(rcol,gcol,bcol);
+      noStroke();
+      circle(i+25,j+25,20);
+    }
+  }
 
 }
--- TestProject/sketch.js
+++ TestProject/sketch.js
@@ -1,15 +1,52 @@
+let xpos = 0;
+let ypos = 150;
+let xballspeed = 10;
+let yballspeed = 7;
+
 
 function setup() {
   // put setup code here
-createCanvas(200,200);
-background(200,10,120);
-
+  createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  //Put draw code here
-  fill(30,40,200);
-  circle(100,100,70);
+  // put drawing code here
+  background(255,179,168);
+  push();
+  rotate(45);
+  fill(190,231,233);
+  noStroke();
+  rect(500,100,250,250);
+  pop();
+  fill(249,205,173);
+  noStroke();
+  circle(700,700,900);
+  fill(180,76,108);
+  circle(xpos,ypos,30);
+
+  ypos = ypos + yballspeed;
+  xpos = xpos + xballspeed;
+
+if (ypos > height){
+  yballspeed = -yballspeed;
+}
+
+if (ypos < 0){
+  yballspeed = -yballspeed;
+}
+
+if (xpos>width){
+  xballspeed = -xballspeed;
+}
+
+if (xpos < 0){
+  xballspeed = -xballspeed;
+}
+
+
+
+
 
 
 }
