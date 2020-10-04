39c3cf91426c808a3d3b9c1579a8ac1d996eb729
Zhouyiwen <Zhouyiwen@ZhouyiwendeMacBook-Pro.local>
2020-10-04 17:22:00 -0400

    thrid

--- Project 1/sketch.js
+++ Project 1/sketch.js
@@ -1,24 +1,23 @@
+let i=random(0,width);
+let j=random(0,height);
+let k=random(0,width);
+let l=random(0,height);
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
   background(255,179,168);
+
 }
 
 function draw() {
   // put drawing code here
-  for(let i=random(0,width);){
-    for(let j=random(0,height);){
-      for(let k=random(0,width);){
-        for(let l=random(0,height);){
+
       let rcol=random(150,255);
       let gcol=random(0,255);
       let bcol=random(100,233);
       fill(rcol,gcol,bcol);
       line(i,j,k,l);
-      }
-     }
-    }
-  }
+
 
 
 }
