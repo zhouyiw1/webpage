53189fb4d512eab28fe5f97a8adb312889c19bb0
zhouyw1 <zhouyiw1@msu.edu>
2020-11-19 15:03:43 +0300

    Project 3

--- Noise/sketch.js
+++ Noise/sketch.js
@@ -1,22 +1,58 @@
-let hornsound;
+var k;
+var n;
+var m;
 
-function preload(){
-  hornsound = loadSound('https://zhouyiw1.github.io/webpage/SoundStuff/Sound.mp3')
-}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(100,100,50);
+  k = 0;
+  n = 0;
+  m = 0;
 
-function setup(){
-  createCanvas(500,500);
-  background(120,0120);
-  hornsound.playMode('restart');
+  }
 
-}
+function draw() {
+  // in this case, we fade the background and in the process give it a low capacity
+  //positions and colors of the shapes change
+  background(0, 5);
 
-function draw(){
+  var x = width * noise(n);
+  var y = height * noise(n+2);
+  //having different colors during the iteration
+  var r = 255 * noise(n+30);
+  var g = 255 * noise(n+67);
+  var b = 255 * noise(n+55);
 
-  fill(120,120,0);
-  circle(200,200,40);
+  noStroke();
+  fill(r, g, b);
+  ellipse(x, y, 70, 70);
+//clowest moving shape
+  n = n + 0.009;
 
-  
+  var x = width * noise(k);
+  var y = height * noise(k+15);
+  //having different colors during the iteration
+  var r = 255 * noise(k+23);
+  var g = 255 * noise(k+47);
+  var b = 255 * noise(k+92);
 
+  noStroke();
+  fill(r, g, b);
+  ellipse(x, y, 30, 100);
+
+  k = k + 0.005;
+
+  var x = width * noise(m);
+  var y = height * noise(m+50);
+  //having different colors during the iteration
+  var r = 255 * noise(m+56);
+  var g = 255 * noise(m+33);
+  var b = 255 * noise(m+5);
+
+  noStroke();
+  fill(r, g, b);
+  square(x, y, 30,);
+//fastest moving shape
+  m = m + 0.002;
 
 }
--- Project 3/sketch.js
+++ Project 3/sketch.js
@@ -1,44 +1,58 @@
-//Manipulation of four images to create an artistic impression
-
-let img;
-let img1;
-let img2;
-let img3;
-
-//images represent bird species in their natural habitat
-
-function preload(){
-  img=loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Pterocnemia_pennat_%28zoo_Zurich%29.jpg/1200px-Pterocnemia_pennat_%28zoo_Zurich%29.jpg')
-
-  img1=loadImage('https://upload.wikimedia.org/wikipedia/commons/0/0d/Loriculus_vernalis_-Ganeshgudi%2C_Karnataka%2C_India_-male-8-1c.jpg')
-
-  img2=loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Bubo_bubo_winter_1.jpg/220px-Bubo_bubo_winter_1.jpg')
-
-  img3=loadImage('https://upload.wikimedia.org/wikipedia/commons/9/92/%E2%99%82_Common_Kingfisher_%28Alcedo_atthis%29_Photograph_By_Shantanu_Kuveskar%2C_Mangaon%2C_Maharashtra%2C_India.jpg')
-}
+var k;
+var n;
+var m;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    }
-
-function draw() {
-  background(50,150,150);
-  //blending of pixels in display window
-  //all the images have their pixels blended on the original with an         exception of image 3
-
-  image(img,400,200,200,200);
-  blend(img,140,190,200,200,0,0,200,200,BURN);//head isolated from the       original image and darker areas applied
-
-  image(img1,200,0,200,200);
-  blend(img1,200,0,200,200,200,0,200,200,DIFFERENCE);//colors subtracted     from underlying image
-
-  image(img2,0,200,200,200);
-  blend(img2,0,200,200,200,0,200,200,200,ADD);//colors subtracted     from underlying image
-
-  image(img3,200,200,200,200);
-
-  //Isolating the beak from image 3
-  //The isolated image is wider than the original to minimize distortion.
-  blend(img3,150,210,300,100,400,0,260,100,BURN);
+  background(100,100,50);
+  k = 0;
+  n = 0;
+  m = 0;
 
   }
+
+function draw() {
+  // in this case, we fade the background and in the process give it a low capacity
+  //positions and colors of the shapes change
+  background(0, 5);
+
+  var x = width * noise(n);
+  var y = height * noise(n+2);
+  //having different colors during the iteration
+  var r = 255 * noise(n+30);
+  var g = 255 * noise(n+67);
+  var b = 255 * noise(n+55);
+
+  noStroke();
+  fill(r, g, b);
+  ellipse(x, y, 70, 70);
+//clowest moving shape
+  n = n + 0.009;
+
+  var x = width * noise(k);
+  var y = height * noise(k+15);
+  //having different colors during the iteration
+  var r = 255 * noise(k+23);
+  var g = 255 * noise(k+47);
+  var b = 255 * noise(k+92);
+
+  noStroke();
+  fill(r, g, b);
+  ellipse(x, y, 30, 100);
+
+  k = k + 0.005;
+
+  var x = width * noise(m);
+  var y = height * noise(m+50);
+  //having different colors during the iteration
+  var r = 255 * noise(m+56);
+  var g = 255 * noise(m+33);
+  var b = 255 * noise(m+5);
+
+  noStroke();
+  fill(r, g, b);
+  square(x, y, 30,);
+//fastest moving shape
+  m = m + 0.002;
+
+}
