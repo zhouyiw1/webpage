ee05f3c2f9347382f666f015816b9a01bfaf060a
zhouyiw1 <70717773+zhouyiw1@users.noreply.github.com>
2020-11-19 22:50:55 -0500

    Update sketch.js

--- Project35/sketch.js
+++ Project35/sketch.js
@@ -1,52 +1,45 @@
-let xpos = 0;
-let ypos = 150;
-let xballspeed = 10;
-let yballspeed = 7;
+let ypos;
+let yposa;
+let yposb;
+let yposc;
 
+let offset = 0.0;
 
 function setup() {
-  // put setup code here
-  createCanvas(windowWidth,windowHeight);
-  angleMode(DEGREES);
+  createCanvas(windowWidth, windowHeight);
+  //noLoop();
+  frameRate(1);
 }
 
 function draw() {
-  // put drawing code here
-  background(255,179,168);
-  push();
-  rotate(45);
-  fill(190,231,233);
-  noStroke();
-  rect(500,100,250,250);
-  pop();
-  fill(249,205,173);
-  noStroke();
-  circle(700,700,900);
-  fill(180,76,108);
-  circle(xpos,ypos,30);
-
-  ypos = ypos + yballspeed;
-  xpos = xpos + xballspeed;
-
-if (ypos > height){
-  yballspeed = -yballspeed;
-}
-
-if (ypos < 0){
-  yballspeed = -yballspeed;
-}
-
-if (xpos>width){
-  xballspeed = -xballspeed;
-}
-
-if (xpos < 0){
-  xballspeed = -xballspeed;
-}
-
-
-
-
-
-
+  background(80,40,70);
+  
+  for(let i = 0;i < width; i = i + 5){
+    ypos1 = noise(offset)*height;
+    fill(0);
+    circle(i,ypos1,5);
+    offset = offset + 0.009;
+  }
+  
+  for(let i = 0;i < width; i = i + 5){
+    yposa = noise(offset)*height;
+    fill(255,255,255);
+    circle(i,yposa,5);
+    offset = offset + 0.009;
+  }
+  
+  for(let i = 0;i < width; i = i + 5){
+    yposb = noise(offset)*height;
+    fill(0,0,255);
+    circle(i,yposb,5);
+    offset = offset + 0.009;
+  }
+  
+  for(let i = 0;i < width; i = i + 5){
+    yposc = noise(offset)*height;
+    fill(255,10,18);
+    circle(i,yposc,5);
+    offset = offset + 0.009;
+  }
+  
 }
